9060a8e43be66820b351fbc6953bb419c1c4a852
Iliam Pavkovic <iliam.pavkovic@intergalaxy.io>
2021-12-07 16:22:13 -0300

    change components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SidebarContext {
+  genreSelected:number;
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[];
+  clickButton: Function;
+}
+
+export function SideBar(props: SidebarContext) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.clickButton(genre.id)}
+            selected={props.genreSelected === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
